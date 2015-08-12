60e5bb6a05e4a83c1f0bb017014ff11645d5bdd8
Chris Sperandio <chris@sperand.io>
2015-08-11 23:32:15 -0700

    adding first pass at CD/CM implementation - no tests yet

--- Analytics/Integrations/GoogleAnalytics/SEGGoogleAnalyticsIntegration.m
+++ Analytics/Integrations/GoogleAnalytics/SEGGoogleAnalyticsIntegration.m
@@ -91,12 +91,14 @@
     if ([self shouldSendUserId])
         [[[GAI sharedInstance] defaultTracker] set:@"&uid" value:userId];
 
-    // We can set traits though. Iterate over a ll the traits and set them.
+    // We can set traits though. Iterate over all the traits and set them.
     self.traits = traits;
 
     [self.traits enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
       [[[GAI sharedInstance] defaultTracker] set:key value:obj];
     }];
+
+    [self setCustomDimensionsAndMetricsOnDefaultTracker:traits]
 }
 
 - (void)track:(NSString *)event properties:(NSDictionary *)properties options:(NSDictionary *)options
@@ -123,18 +125,24 @@
 
     SEGLog(@"Sending to Google Analytics: category %@, action %@, label %@, value %@", category, event, label, value);
 
+    GAIDictionaryBuilder *hit =
+        [GAIDictionaryBuilder createEventWithCategory:category
+                                              action:event
+                                               label:label
+                                               value:value];
+
+    [self setCustomDimensionsAndMetrics:properties onHit:hit];
+
     // Track the event!
-    [[[GAI sharedInstance] defaultTracker] send:
-                                               [[GAIDictionaryBuilder createEventWithCategory:category
-                                                                                       action:event
-                                                                                        label:label
-                                                                                        value:value] build]];
+    [[[GAI sharedInstance] defaultTracker] send: [hit build]];
 }
 
 - (void)screen:(NSString *)screenTitle properties:(NSDictionary *)properties options:(NSDictionary *)options
 {
     [[[GAI sharedInstance] defaultTracker] set:kGAIScreenName value:screenTitle];
-    [[[GAI sharedInstance] defaultTracker] send:[[GAIDictionaryBuilder createScreenView] build]];
+    GAIDictionaryBuilder *view = [GAIDictionaryBuilder createScreenView];
+    [self setCustomDimensionsAndMetrics:properties onHit:view];
+    [[[GAI sharedInstance] defaultTracker] send:[view build]];
 }
 
 #pragma mark - Ecommerce
@@ -178,6 +186,47 @@
 }
 
 #pragma mark - Private
+
+// event and screen properties are generall hit-scoped dimensions, so we want
+// to set them on the hits, not the tracker
+- (void)setCustomDimensionsAndMetrics:(NSDictionary *)properties onHit:(GAIDictionaryBuilder *)hit
+{
+    NSDictionary *customDimensions = self.settings[@"dimensions"];
+    NSDictionary *customMetrics = self.settings[@"metrics"]
+
+    for (NSString *key in properties) {
+        if ([customDimensions objectForKey:key] != nil) {
+            NSString *dimension = [customDimensions objectForKey:key];
+            [hit set: [properties objectForKey:key]
+              forKey: [GAIFields customDimensionForIndex:[dimension integerValue]]]
+        }
+        if ([customMetrics objectForKey:key] != nil) {
+            NSString *metric = [customMetrics objectForKey:key];
+            [hit set: [properties objectForKey:key]
+              forKey: [GAIFields customMetricForIndex:[metric integerValue]]]
+        }
+    }
+}
+
+// traits are user-scoped dimensions. as such, it makes sense to set them on the tracker
+- (void)setCustomDimensionsAndMetricsOnDefaultTracker:(NSDictionary *)traits
+{
+    NSDictionary *customDimensions = self.settings[@"dimensions"];
+    NSDictionary *customMetrics = self.settings[@"metrics"]
+
+    for (NSString *key in traits) {
+        if ([customDimensions objectForKey:key] != nil) {
+            NSString *dimension = [customDimensions objectForKey:key];
+            [[[GAI sharedInstance] defaultTracker] set: [GAIFields customDimensionForIndex:[dimension integerValue]]
+                                                 value: [traits objectForKey:key]]
+        }
+        if ([customMetrics objectForKey:key] != nil) {
+            NSString *metric = [customMetrics objectForKey:key];
+            [[[GAI sharedInstance] defaultTracker] set: [GAIFields customMetricForIndex:[metric integerValue]]
+                                                 value: [traits objectForKey:key]]
+        }
+    }
+}
 
 - (BOOL)shouldSendUserId
 {
